968a8501718428bd12e60d0efbb5fcd9977d418a
heroazure <1147024669@qq.com>
2015-03-08 00:17:17 +0800

    11
    11

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+//Ext.application({
+//    name: 'myapp',
+//    icon: 'images/icon.png',
+//    glossOnIcon: false,
+//    phoneStartupScreen: 'images/phone_startup.png',
+//    tabletStartupScreen: 'images/tablet_startup.png',
+//    launch: function () {
+//        var panel = Ext.create('Ext.Panel', {
+//            fullscreen: true,
+//            id: 'mypanel',
+//            style: 'color:red',
+//            html: '一个简单的示例面板'
+//        })
+//    }
+//});
+
+
 Ext.application({
-    name: "MyApp",
+    name: 'myapp',
+    icon: 'images/icon.png',
+    glossOnIcon: false,
+    phoneStartupScreen: 'images/phone_startup.png',
+    tabletStartupScreen: 'images/tablet_startup.png',
     launch: function () {
-        alert("Sencha Touch 2.4 框架已被加载！");
+        var panel = Ext.create('Ext.Panel', {
+            //fullscreen: true,
+            id: 'mypanel',
+
+            html: '一个简单的示例面板'
+        });
+        var subpanel = Ext.create('Ext.Panel', {
+            id: 'subpanel',
+            html: '子面板'
+        });
+        Ext.Viewport.add(panel);
+        Ext.ComponentManager.get('mypanel').add(subpanel);//.addCls('classred')
     }
 });
